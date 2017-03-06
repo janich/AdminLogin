aad14bd1f8454a64cfdcd94aee5adac5db57e189
Janich Rasmussen <janich@gmail.com>
2017-03-06 11:22:08 +0100

    Initial commit

---
+++ adminlogin.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * @package     Joomla
+ * @subpackage  System.adminlogin
+ 
+ * @copyright   Copyright (C) 2017 janich.dk, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+defined('_JEXEC') or die;
+
+
+require_once __DIR__ .'/helpers/helper.php';
+
+
+class PlgSystemAdminlogin extends JPlugin
+{
+	protected   $autoloadLanguage   = true;
+    protected   $helper	            = null;
+    protected   $debug	            = null;
+    protected   $key		        = null;
+    
+
+	public function __construct($subject, $config = array())
+	{
+		parent::__construct($subject, $config);
+        
+		$this->helper   = new AdminLoginHelper();
+        $this->debug	= $this->params->get('debug', 0);
+        $this->key	    = $this->params->get('key', md5(mt_rand(0, 99999)));
+	}
+
+
+	public function onGetAdminloginButton($userId = 0)
+	{
+        $link = $this->onGetAdminloginLink($userId);
+		
+		if (!$link) {
+			return '';
+		}
+		
+		return '<a href="'. $link .'" target="_blank" class="btn btn-success btn-mini btn-adminlogin btn-adminlogin-'. (int) $userId .'" title="'. JText::_('PLG_SYSTEM_ADMINLOGIN_BTN_TITLE') .'">'. JText::_('PLG_SYSTEM_ADMINLOGIN_BTN_TEXT') .'</a>';
+	}
+	
+	
+	public function onGetAdminloginLink($userId = 0)
+	{
+        return $this->helper->generateLoginLink($userId, $this->key);
+	}
+    
+    
+	public function onAfterRender()
+	{
+        $app = JFactory::getApplication();
+        
+		if (!$app->isAdmin())
+		{
+			return true;
+		}
+
+		if (!($app->input->get('option', '', 'cmd') == 'com_users' && $app->input->get('view', '', 'cmd') == 'users'))
+        {
+            return true;
+        }
+
+        $this->helper->addLoginButtons($this->key);
+	}
+    
+    
+	public function onAfterInitialise()
+	{
+        $app = JFactory::getApplication();
+        
+		if (!$app->isSite())
+		{
+			return true;
+		}
+
+		$hash = $app->input->get('adminlogin', false);
+		if (!$hash)
+		{
+			return true;
+		}
+        
+        try 
+        {
+            // Test secret
+            if ($hash != md5($this->key))
+            {
+                throw new Exception('PLG_SYSTEM_ADMINLOGIN_ERR_INVALIDKEY');
+            }
+            
+            // Test user
+            $username   = $app->input->get('username', '', 'raw');
+            $password   = $app->input->get('password', '', 'raw');
+            $user       = $this->helper->matchUser($username, $password);
+            if (!$user)
+            {
+                throw new Exception('PLG_SYSTEM_ADMINLOGIN_ERR_USERNOTFOUND');
+            }
+            
+            if ($user->block)
+            {
+                throw new Exception('PLG_SYSTEM_ADMINLOGIN_ERR_USERBLOCKED');
+            }
+            
+            // Test backend session
+            $session_id = $this->helper->getBackendSessionId();
+            if (!$session_id)
+            {
+                throw new Exception('PLG_SYSTEM_ADMINLOGIN_ERR_NOTLOGGEDIN');
+            }
+    
+            // Test backend session login
+            $session = $this->helper->getUserSession($session_id);
+            if (!$session)
+            {
+                throw new Exception('PLG_SYSTEM_ADMINLOGIN_ERR_NOSESSION');
+            }
+    
+            // Do magic
+            if (!$this->helper->loginUser($user->id))
+            {
+                throw new Exception('PLG_SYSTEM_ADMINLOGIN_ERR_UNKNOWN');
+            }
+
+            $app->redirect('index.php', JText::sprintf('PLG_SYSTEM_ADMINLOGIN_SUCCESS', $user->username));
+        }
+        catch (Exception $e)
+        {
+			$error = $e->getMessage();
+			if (substr($error, 0, 21) == 'PLG_SYSTEM_ADMINLOGIN') 
+			{
+				$error = JText::_($error);
+			}
+			
+			if ($this->debug)
+            {
+                $app->enqueueMessage($error, 'error');
+            }
+        }
+
+		return true;
+	}
+}
---
+++ helpers/helper.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * @package     Joomla
+ * @subpackage  System.adminlogin
+ 
+ * @copyright   Copyright (C) 2017 janich.dk, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+defined('_JEXEC') or die;
+
+
+class AdminLoginHelper
+{
+	protected $app		= null;
+	protected $dbo		= null;
+	protected $input	= null;
+
+
+	public function __construct()
+	{
+        $this->app		= JFactory::getApplication();
+		$this->dbo		= JFactory::getDbo();
+		$this->doc		= JFactory::getDocument();
+	}
+
+
+	public function generateLoginLink($userId = 0, $key = '')
+	{
+		$user = JFactory::getUser($userId);
+
+		if (!$user || !$user->get('id', 0) || $user->get('block'))
+		{
+			return false;
+		}
+
+		$parts = array(
+			'adminlogin=' . md5($key),
+			'username=' .   $user->get('username'),
+			'password=' .   md5($user->get('password'))
+		);
+
+		return JUri::root() . '?' . implode('&', $parts);
+	}
+
+
+    public function addLoginButtons($key = '')
+    {
+        $html = $this->app->getBody();
+
+        if (preg_match_all('/task=user.edit&amp;id=[0-9]*/', $html, $matches)) {
+            $matches = $matches[0];
+        }
+
+        $head   = array();
+        $head[] = '<style type="text/css">.btn-adminlogin { float: right; background: #46a546; padding: 2px 5px 2px 6px; border-radius: 2px; font-weight: normal; color: white; font-size: 12px;} .btn-adminlogin:hover { background: #555; text-decoration: none; color: white; }</style>';
+        $head[] = '<script type="text/javascript">';
+        $head[] = 'function addLoginButton(id, link) {';
+        $head[] = 'jQuery(\'#userList a[href$="task=user.edit&id=\'+ id +\'"]\').parent().parent().next().append(\'<a class="icon-shuffle btn-adminlogin" title="'. JText::_('PLG_SYSTEM_ADMINLOGIN_BTN_TITLE') .'" href="\'+ link +\'" target="_blank"> </a>\');';
+        $head[] = '}';
+
+        $head[] = 'jQuery(document).ready(function() {';
+
+        foreach ($matches as $match)
+        {
+            $parts = (array) explode('=', $match);
+            $id = (int) end($parts);
+
+            if ($id) 
+            {
+                $head[] = 'addLoginButton(' . $id . ', \'' . $this->generateLoginLink($id, $key) . '\');';
+            }
+        }
+
+        $head[] = '});';
+        $head[] = '</script>';
+
+        $html = str_ireplace('</head>', implode("\n", $head) . '</head>', $html);
+
+        $this->app->setBody($html);
+    }
+
+
+    public function matchUser($username = '', $password = '')
+    {
+        $query = $this->dbo->getQuery(true)
+            ->select('*')
+            ->from('#__users')
+            ->where('`username` = '. $this->dbo->quote($username))
+            ->where('MD5(`password`) = '. $this->dbo->quote($password));
+        $this->dbo->setQuery($query);
+        return $this->dbo->loadObject();
+    }
+
+
+    public function getBackendSessionId()
+    {
+        $name = md5(JApplicationHelper::getHash('administrator'));
+        return $this->app->input->cookie->get($name, '');
+    }
+
+
+    public function getUserSession($session_id = '')
+    {
+        $q = $this->dbo->getQuery(true)
+            ->select('*')
+            ->from('#__session')
+            ->where('`session_id` = '. $this->dbo->quote($session_id))
+            ->where('`client_id` = 1')
+            ->where('`guest` = 0');
+        $this->dbo->setQuery($q);
+        return $this->dbo->loadObject();
+    }
+
+
+    public function loginUser($userId = 0)
+    {
+        $user       = JFactory::getUser($userId);
+        $session    = JFactory::getSession();
+
+        $user->set('guest', 0);
+        $user->set('aid', 1);
+
+        $session->set('user', $user);
+
+        $table = JTable::getInstance('session');
+        $table->load($session->getId());
+
+        $table->guest 		= 0;
+        $table->username 	= $user->get('username');
+        $table->userid 		= (int) $user->get('id', 0);
+
+        $table->update();
+
+        return true;
+    }
+}
